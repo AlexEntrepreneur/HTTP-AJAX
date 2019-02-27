bd0e88efabb99d424d025197d225300b634d03ea
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-27 12:55:15 +0000

    Get Friends From API + Handle Errors

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    this.getFriendsData();
+  }
+
+  getFriendsData = () => {
+    this.resetErrorState();
+    axios.get('http://localhost:5000/friends')
+      .then(response => this.setFriendDataToState(response.data))
+      .catch(err => this.setErrorToState(err.message));
+  }
+
+  setFriendDataToState = data => {
+    this.setState({ friends: data })
+  }
+
+  setErrorToState = err => {
+    this.setState({ error: err });
+  }
+
+  resetErrorState = () => {
+    this.setState({ error: null });
+  }
+
   render() {
     return (
       <div className="App">
-        
+
       </div>
     );
   }
